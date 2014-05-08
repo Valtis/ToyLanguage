d38fc169ce084c4733312c1608dbb3730e6c7fed
Valtis <erkka.kaaria@helsinki.fi>
2014-05-08 10:01:03 +0300

    git add with -A flag to remove moved\renamed files

--- ToyLanguage/OperationType.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-enum class OperationType { BLOCK, FUNCTION_CALL, ADD, SUB, MUL, DIV, ASSIGNMENT, VARIABLE_DECLARATION, VALUE };
--- ToyLanguage/StringUtility.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-namespace Utility
-{
-  std::vector<std::string> Tokenize(const std::string &text, const std::string &delimiters);
-  std::string Trim(const std::string &line);
-}
